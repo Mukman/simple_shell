751a7d6129c8053fa2ce8d29f3b9e8b1c24cd2ef
Bereket <beaky2193@gmail.com>
2023-12-13 23:33:07 +0300

    BTM

--- BTM.c
+++ BTM.c
@@ -1,65 +1,65 @@
 #include "shell.h"
 
 /**
-* interactive - returns true if shell is interactive mode
-* @addres: struct address
+* active - returns true if shell is active mode
+* @info: struct address
 *
-* Return: 1 if interactive mode, 0 otherwise
+* Return: 1 if active mode, 0 otherwise
 */
-int interactive(info_t *addres)
+int active(info_t *info)
 {
-return (isatty(STDIN_FILENO) && addres->readfd <= 2);
+return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
 
 /**
-* is_delimeter - checks if character is a delimeter
-* @x: the char to check
-* @delimeter: the delimeter string
+* delim - checks if character is a delimeter
+* @c: the char to check
+* @delim: the delimeter string
 * Return: 1 if true, 0 if false
 */
-int is_delimeter(char x, char *delimeter)
+int delim(char c, char *delim)
 {
-while (*delimeter)
-if (*delimeter++ == x)
+while (*delim)
+if (*delim++ == c)
 return (1);
 return (0);
 }
 
 /**
-*_isalphabet - checks for alphabetic character
-*@x: The character to input
-*Return: 1 if c is alphabetic, 0 otherwise
+*alpha - checks for alphabetic character
+*@d: The character to input
+*Return: 1 if d is alphabetic, 0 otherwise
 */
 
-int _isalphabet(int x)
+int alpha(int d)
 {
-if ((x >= 'a' && x <= 'z') || (x >= 'A' && x <= 'Z'))
+if ((d >= 'a' && d <= 'z') || (d >= 'A' && d <= 'Z'))
 return (1);
 else
 return (0);
 }
 
 /**
-*conv_int - converts a string to an integer
-*@n: the string to be converted
+*__atoi - converts a string to an integer
+*@s: the string to be converted
 *Return: 0 if no numbers in string, converted number otherwise
 */
 
-int conv_int(char *n)
+int __atoi(char *s)
 {
 int i, sign = 1, flag = 0, output;
 unsigned int result = 0;
 
-for (i = 0;  n[i] != '\0' && flag != 2; i++)
+for (i = 0;  s[i] != '\0' && flag != 2; i++)
 {
-if (n[i] == '-')
+if (s[i] == '-')
 sign *= -1;
 
-if (n[i] >= '0' && n[i] <= '9')
+if (s[i] >= '0' && s[i] <= '9')
 {
 flag = 1;
 result *= 10;
-result += (n[i] - '0');
+result += (s[i] - '0');
 }
 else if (flag == 1)
 flag = 2;
